e2ce227704cb4b9886e7f50ba4ff09c9a7237ded
GTsan <sofronovae123456@gmail.com>
2024-04-21 12:52:13 +0300

    beautify

--- web/public/wrapper.js
+++ web/public/wrapper.js
@@ -15,27 +15,15 @@ function bufferToHex(buffer) {
 async function wrapper(JSON_KEY_DATA, PASSWD) {
     const hashFull = await hashData(PASSWD);
     const hash = hashFull.slice(0,24);
-    // console.log(hash);
     return cryptoContainers.encryptContainer(JSON_KEY_DATA, hash);
 }
 
 async function unwrapper(ENCONTAINER, PASSWD) {
-    // console.log(md5(PASSWD).slice(0,24));
     const hashFull = await hashData(PASSWD);
     const hash = hashFull.slice(0,24);
-    // console.log(hash);
     try {
         return cryptoContainers.decryptContainer(ENCONTAINER, hash);
     } catch (e) {
         alert("Неверный пароль");
     }
 }
-
-async function test1() {
-    let test = await wrapper("Hello", "kali");
-    console.log(test);
-// console.log(test.then(function (result)));
-    console.log(await unwrapper(test, "kali"));
-}
-
-// test1()
